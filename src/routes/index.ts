e8b8733beead2395ab316f05c1658219ecd47c72
Alexandre Junior <alejunior.dev@gmail.com>
2024-07-10 23:27:18 -0300

    feat: :sparkles: add new routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,7 @@
 export * from "./confirm-participant";
 export * from "./confirm-trip";
 export * from "./create-trip";
+export * from "./create-activity";
+export * from "./get-activities";
+export * from "./create-link";
+export * from "./get-links";
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,15 @@ import {
 } from "fastify-type-provider-zod";
 
 import cors from "@fastify/cors";
-import { confirmParticipant, confirmTrip, createTrip } from "./routes";
+import {
+  confirmParticipant,
+  confirmTrip,
+  createActivity,
+  createLink,
+  createTrip,
+  getActivities,
+  getLinks,
+} from "./routes";
 
 const app = fastify();
 
@@ -20,6 +28,10 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running! 🚀");
