880420935aeb68fffb34adf8a2ebe74b5d1af154
Alexandre Junior <alejunior.dev@gmail.com>
2024-07-10 23:26:34 -0300

    feat: :sparkles: create link routes

---
+++ src/routes/get-links.ts
@@ -0,0 +1,31 @@
+import { dayjs, prisma } from "@/lib";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export async function getLinks(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/trips/:tripId/links",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { tripId } = request.params;
+
+      const trip = await prisma.trip.findUnique({
+        where: { id: tripId },
+        include: { links: true },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found");
+      }
+
+      return { links: trip.links };
+    }
+  );
+}
